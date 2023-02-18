0484d1758846417155a609190439d32e90c6c1bd
David Grammatico <david.lugdunum@gmail.com>
2023-02-18 19:23:18 +0100

    add firebase config

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -1,5 +1,21 @@
+'use client'
+
+import { signIn } from 'next-auth/react'
+import Image from 'next/image'
+
 function Login() {
-  return <div>Login</div>
+  return (
+    <div className="bg-[conic-gradient(at_left,_var(--tw-gradient-stops))] from-rose-100 to-teal-100 h-screen flex flex-col items-center justify-center text-center">
+      <Image src="/assets/logo.png" width={300} height={300} alt="logo" />
+      <button
+        className="font-bold text-3xl animate-pulse text-green-500"
+        type="button"
+        onClick={() => signIn('google')}
+      >
+        Connectez-vous pour utiliser ChatGPT
+      </button>
+    </div>
+  )
 }
 
 export default Login
--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,7 +1,11 @@
-import React from 'react'
+'use client'
+
+import { useSession, signOut } from 'next-auth/react'
+import Image from 'next/image'
 import NewChat from './NewChat'
 
 function SideBar() {
+  const { data: session } = useSession()
   return (
     <div className="-p-2 flex flex-col h-screen">
       <div className="flex-1">
@@ -12,6 +16,16 @@ function SideBar() {
           {/* Map through the ChatRows */}
         </div>
       </div>
+      {session && (
+        <Image
+          onClick={() => signOut()}
+          src={session.user?.image ?? '/assets/user.png'}
+          alt="photo de profil"
+          width={50}
+          height={50}
+          className="rounded-full cursor-pointer h-12 w-12 mx-auto mb-2 hover:opacity-50"
+        />
+      )}
     </div>
   )
 }
